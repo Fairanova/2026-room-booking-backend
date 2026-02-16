73a153ae43326bd85a290d9c260db08ed8f6a308
Fairanova <112160275+Fairanova@users.noreply.github.com>
2026-02-17 02:50:43 +0700

    bugfix: fix database migration issues (AddRejectionReasonToBooking)

---
+++ Migrations/20260216194709_AddRejectionReasonToBooking.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomBookingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRejectionReasonToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReason",
+                table: "Bookings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionReason",
+                table: "Bookings");
+        }
+    }
+}
